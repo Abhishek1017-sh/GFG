00ce3adc6c2b888149bc663183f8f5c9ad835041
Abhishek1017-sh <as20041009@gmail.com>
2025-08-19 16:24:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert a node in a BST/insert-a-node-in-a-bst.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Function to insert a node in a BST.
+    Node insert(Node root, int key) {
+        // your code here
+        if(root==null) return new Node(key);
+        if(key<root.data){
+            root.left=insert(root.left,key);
+        }
+        else if(key>root.data){
+            root.right=insert(root.right,key);
+        }
+        return root;
+    }
+}
